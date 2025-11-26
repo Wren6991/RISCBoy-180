e608cf926d4c99571def1bd19153f1bd383ec57d
Luke Wren <wren6991@gmail.com>
2025-11-26 00:55:22 +0000

    Fiddling with filter coefficients

--- hdl/apu/aout/filter.py
+++ hdl/apu/aout/filter.py
@@ -3,38 +3,38 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-num_taps = 33
-sample_rate = 48e3 * 8
+num_taps = 63
+upsample = 16
+sample_rate = 48e3 * upsample
 cut_off = 22e3
 
 h = signal.firwin(num_taps, cut_off, fs=sample_rate)
 
 # Quantise the response. Magic number makes the worst-case sum of absolutes
 # exactly 128 (with 7 zeroes stuffed between each sample)
-quantise = 2 ** 7 * 8
-magic = 0.84
+qbits = 8
+quantise = 2 ** qbits * upsample
+magic = 0.87
 b = np.array(list(round(x * quantise * magic) for x in h))
 
 def skipskum(b, i):
-	if i == 0:
-		return abs(b[0]) + abs(b[8]) + abs(b[16]) + abs(b[24]) + abs(b[32])
-	else:
-		return abs(b[i]) + abs(b[i + 8]) + abs(b[i + 16]) + abs(b[i + 24])
+	return sum(abs(b[j]) for j in range(i, num_taps, upsample))
 
-for i in range(0, 9):
+for i in range(0, upsample + 1):
 	print(skipskum(b, i))
-	assert skipskum(b, i) <= 128
+	assert skipskum(b, i) <= 2 ** qbits
 
 for i, x in enumerate(b):
-	print(f"// {i:<2} : {'+' if x >= 0 else '-'}7'b{abs(x):07b}")
+	print(f"\t9'h{x & 0x1ff:03x},")
 
 # plt.plot(h, '.-')
-# plt.plot(b / (QUANTISE * MAGIC), '.-')
+# plt.plot(b / (quantise * magic), '.-')
 # plt.show()
 
-H = np.abs(np.fft.fft(b / 1024, 1024))
+H = np.abs(np.fft.fft(b / (quantise * magic), 1024))
 H = np.fft.fftshift(H)
 logH = 20 * np.log10(H)
 w = np.linspace(-sample_rate/2, sample_rate/2, len(H))
 plt.plot(w, logH, '.-')
+plt.grid()
 plt.show()
